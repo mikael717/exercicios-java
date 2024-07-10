efa9863bab7aba1caf0a32b106623a3a711a6efb
Mikael Maia <mika.sjj@hotmail.com>
2024-07-10 11:09:39 -0300

    apenas uma mudança

--- exDiagonalNeg.java
+++ exDiagonalNeg.java
@@ -23,7 +23,7 @@ public class exDiagonalNeg {
         for(int i=0; i<n; i++){
             System.out.print(mat[i][i] + " ");
         }
-        System.out.println(); //quebra de linha
+        System.out.println(); //apennas para quebra de linha
 
         int negativos = 0;
 

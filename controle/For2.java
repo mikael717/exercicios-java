9987fc8bf3b16654a7abb8aefe3dc01b520fb08a
Mikael Maia <mika.sjj@hotmail.com>
2025-02-16 09:16:21 -0300

    DesFor2

---
+++ controle/For2.java
@@ -0,0 +1,18 @@
+package controle;
+
+public class For2 {
+
+	public static void main(String[] args) {
+		
+	/*	for (int i=0 ; i<=10; i++) {
+			
+			if(i%2 == 0) {
+				System.out.println(i);
+			}
+		}*/
+	for (int i= 10; i >=0; i-=2 ) { //atribuição subtrativa; decrementar a variável do contador 2 em 2;
+			System.out.println(i);
+		}
+	}
+		
+}

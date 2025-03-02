649588f57225621306d52436a0ee79a56199eb5d
Mikael Maia <mika.sjj@hotmail.com>
2025-03-02 10:55:10 -0300

    primeiro trauma

---
+++ classe/PrimeiroTrauma.java
@@ -0,0 +1,12 @@
+package classe;
+
+public class PrimeiroTrauma {
+
+	int a =3; // atributo que pertence à instância;	
+	
+	public static void main(String[] args) { //static pertence à classe
+		
+		PrimeiroTrauma p = new PrimeiroTrauma();
+		System.out.println(p.a + " = valor de a");
+	}
+}

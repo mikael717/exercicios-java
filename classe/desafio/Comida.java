83be642e51322e50252cfa2664c2b3f70601cca8
Mikael Maia <mika.sjj@hotmail.com>
2025-03-03 19:22:06 -0300

    food

---
+++ classe/desafio/Comida.java
@@ -0,0 +1,15 @@
+package classe.desafio;
+
+public class Comida {
+
+	String nomeComida; //passando direto sem valor de string
+	double pesoComida;
+	
+	Comida (){
+		
+	}
+	Comida(String nomeComida, double pesoComida){
+		this.nomeComida = nomeComida;
+		this.pesoComida = pesoComida;
+	}
+}

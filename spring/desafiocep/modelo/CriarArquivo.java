3ad94b2bbcd6db14904d0971af6478766f17c3b4
Mikael Maia <mika.sjj@hotmail.com>
2025-03-28 10:41:49 -0300

    create file

---
+++ spring/desafiocep/modelo/CriarArquivo.java
@@ -0,0 +1,19 @@
+package desafios.desafiocep.modelo;
+
+//import com.google.gson.Gson;
+//import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CriarArquivo {
+
+    public void criaJson(Endereco endereco) throws IOException {
+        //Gson gson = new GsonBuilder()
+               // .setPrettyPrinting()
+               // .create();
+        FileWriter writer = new FileWriter(endereco.cep() + ".json");
+        //writer.write(gson.toJson(endereco));
+        writer.close();
+    }
+}

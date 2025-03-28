8123675d4bdfda2994dad807035a28aede2428db
Mikael Maia <mika.sjj@hotmail.com>
2025-03-28 10:43:00 -0300

    main

---
+++ spring/desafiocep/principal/Main.java
@@ -0,0 +1,35 @@
+/*package desafios.desafiocep.principal;
+
+//import com.google.gson.Gson;
+//import com.google.gson.GsonBuilder;
+import desafios.desafiocep.modelo.ConsultaCep;
+import desafios.desafiocep.modelo.CriarArquivo;
+import desafios.desafiocep.modelo.Endereco;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite seu CEP: ");
+        var cep = sc.nextLine();
+
+        try {
+
+            ConsultaCep consultaCep = new ConsultaCep();
+            //Endereco endereco = consultaCep.buscaEndereco(cep);
+           // System.out.println(endereco);
+
+            CriarArquivo novoArquivo = new CriarArquivo();
+//novoArquivo.criaJson(endereco);
+//
+      //  } catch (RuntimeException | IOException e) {
+       //     System.out.println(e.getMessage());
+       // }
+        System.out.println("finalização da aplicaão");
+
+
+    }
+}*/

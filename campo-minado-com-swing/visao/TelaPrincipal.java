613e8404215bf72a7834c60ef2f890f70d94833d
Mikael Maia <mika.sjj@hotmail.com>
2025-04-02 13:02:44 -0300

    main screen

---
+++ campo-minado-com-swing/visao/TelaPrincipal.java
@@ -0,0 +1,25 @@
+package br.com.mikael.cm.visao;
+
+import javax.swing.JFrame;
+
+import br.com.mikael.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class TelaPrincipal extends JFrame{
+
+	public TelaPrincipal() {
+		Tabuleiro tabuleiro = new Tabuleiro (16, 30, 25);
+		add(new PainelTabuleiro(tabuleiro));
+		
+		setTitle("Campo Minado");
+		setSize(690,438);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new TelaPrincipal();
+	}
+}
+//bugs eliminated 

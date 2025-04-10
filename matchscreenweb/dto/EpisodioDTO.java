a27f87f8bdc58a09e7cce1bc59ad3577f3c98d9e
Mikael Maia <mika.sjj@hotmail.com>
2025-04-10 17:23:35 -0300

    dto-episode

---
+++ matchscreenweb/dto/EpisodioDTO.java
@@ -0,0 +1,7 @@
+package br.com.alphaflix.matchscreenweb.dto;
+
+public record EpisodioDTO(Integer temporada,
+                          Integer numeroEpisodio,
+                          String titulo) {
+
+}

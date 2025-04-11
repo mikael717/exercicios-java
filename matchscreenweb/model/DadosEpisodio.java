caa8d51701aef463362a05d6dcc20e12d659ca1d
Mikael Maia <mika.sjj@hotmail.com>
2025-04-11 16:14:03 -0300

    data-episode

---
+++ matchscreenweb/model/DadosEpisodio.java
@@ -0,0 +1,11 @@
+package br.com.alphaflix.matchscreenweb.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosEpisodio(@JsonAlias("Title") String titulo,
+                            @JsonAlias("Episode") Integer numero,
+                            @JsonAlias("imdbRating") String avaliacao,
+                            @JsonAlias("Released") String dataLancamento) {
+}

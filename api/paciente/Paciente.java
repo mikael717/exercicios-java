30d9a2de1aa89a81ab1be660b2b1f11c813ea891
Mikael Maia <mika.sjj@hotmail.com>
2025-05-22 08:04:30 -0300

    patient

---
+++ api/paciente/Paciente.java
@@ -0,0 +1,83 @@
+package med.voll.api.paciente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Getter
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Paciente")
+@Table(name = "pacientes")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String telefone;
+
+    @Embedded
+    private Endereco endereco;
+    private boolean ativo;
+
+    public Paciente(){}
+
+    public Paciente(DadosCadastroPaciente dados){
+        this.ativo = true;
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.cpf = dados.cpf();
+        this.endereco = new Endereco(dados.endereco());
+    }
+
+    public Long getId(){
+        return id;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+        if(dados.nome() != null){
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null){
+            endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
+
+    public void inativar() {
+        this.ativo = false;
+    }
+}

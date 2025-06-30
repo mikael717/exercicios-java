86451248d1341021e0924aba3621cf83b496bbd6
Mikael Maia <mika.sjj@hotmail.com>
2025-06-30 09:42:46 -0300

    consult-repository

---
+++ api/domain/consulta/ConsultaRepository.java
@@ -0,0 +1,13 @@
+package med.voll.api.domain.consulta;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+    boolean existsByMedicoIdAndDataAndMotivoCancelamentoIsNull(Long idMedico, @Future LocalDateTime data);
+
+    boolean existsByPacienteIdAndDataBetween( Long idPaciente, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
+}

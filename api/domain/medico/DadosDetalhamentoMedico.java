855311ab8f585f5598cf79685480e05feb4d1cc7
Mikael Maia <mika.sjj@hotmail.com>
2025-07-03 09:26:22 -0300

    datails-doctor

--- api/domain/medico/DadosDetalhamentoMedico.java
+++ api/domain/medico/DadosDetalhamentoMedico.java
@@ -1,8 +1,11 @@
 package med.voll.api.domain.medico;
 
-public record DadosDetalhamentoMedico(String nome, String email, String telefone, String crm, Especialidade especialidade) {
+import med.voll.api.domain.endereco.DadosEndereco;
+import med.voll.api.domain.endereco.Endereco;
+
+public record DadosDetalhamentoMedico(String nome, String email, String telefone, String crm, Especialidade especialidade, Endereco endereco) {
 
     public DadosDetalhamentoMedico(Medico medico){
-        this(medico.getNome(), medico.getEmail(), medico.getTelefone(), medico.getCrm(), medico.getEspecialidade());
+        this(medico.getNome(), medico.getEmail(), medico.getTelefone(), medico.getCrm(), medico.getEspecialidade(), medico.getEndereco());
     }
 }

f4f8c55654f38ae3ed438dff78ecce671facd09a
Mikael Maia <mika.sjj@hotmail.com>
2025-08-02 18:53:33 -0300

    shelter-repository

---
+++ adopet/api/repository/AbrigoRepository.java
@@ -0,0 +1,11 @@
+package br.com.alura.adopet.api.repository;
+
+import br.com.alura.adopet.api.model.Abrigo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AbrigoRepository extends JpaRepository<Abrigo, Long> {
+
+    boolean existsByNomeOrTelefoneOrEmail(String nome, String telefone, String email);
+
+    Abrigo findByNome(String nome);
+}

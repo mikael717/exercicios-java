ac2d88ab5cc9eaedffd8e4363bb729f781f64d98
Mikael Maia <mika.sjj@hotmail.com>
2025-08-13 07:30:39 -0300

    validation-shelter-with-adoption-limit

---
+++ adopet/api/validacoes/ValidacaoTutorComLimiteDeAdocoes.java
@@ -0,0 +1,36 @@
+package br.com.alura.adopet.api.validacoes;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Adocao;
+import br.com.alura.adopet.api.model.StatusAdocao;
+import br.com.alura.adopet.api.model.Tutor;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import br.com.alura.adopet.api.repository.TutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ValidacaoTutorComLimiteDeAdocoes implements ValidacaoSolicitacaoAdocao {
+
+    @Autowired
+    private AdocaoRepository adocaoRepository;
+
+
+    @Autowired
+    private TutorRepository tutorRepository;
+
+    public void validar(SolicitacaoAdocaoDto dto) {
+        boolean tutorNoLimiteDeAdocoes = adocaoRepository.existsByTutorIdAndStatus(dto.idTutor(), StatusAdocao.APROVADO);
+
+        int contador = 0;
+        if (tutorNoLimiteDeAdocoes) {
+            contador++;
+        }
+        if (contador == 5) {
+            throw new ValidacaoException("Tutor chegou ao limite máximo de 5 adoções!");
+        }
+    }
+}
